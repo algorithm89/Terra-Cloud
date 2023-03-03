61fcf732d17635c526c85379e0e4213bfa5a100b
superkolobok <alexei.go89@gmail.com>
2023-03-03 14:03:00 -0500

    A

--- module/ec2/instances.tf
+++ module/ec2/instances.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "WEB" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   subnet_id     = "${var.subnet_id}"
-
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
  
 }
 
